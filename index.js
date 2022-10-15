19bccde753cf8413cae924fad39c12b845825b47
David Martos <davidignaciomartos@gmail.com>
2022-10-15 21:08:31 +0200

    Get all persons and info of the phonebook

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const express = require('express')
 const app = express()
 
-app.get('/',(request,response) =>{
-    response.send("<h1>Hello World</h1>")
+const persons = [
+    {
+        id: 1,
+        name: 'Arto Hellas',
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: 'Ada Lovelace',
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: 'Dan Abramov',
+        number: "12-43-234345"
+    },
+    {
+        id: 1,
+        name: 'Mary Poppendick',
+        number: "39-23-6423122"
+    }
+]
+
+
+app.get('/api/persons',(request,response) => {
+    response.json(persons)
+})
+
+app.get('/info',(request,response) => {
+    response.send(
+        `
+            <p>Phonebook has info for ${persons.length} people</p>
+            <br>
+            ${new Date}
+        `
+    )
 })
 
 const PORT = 3001
