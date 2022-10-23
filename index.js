f8fbbe5f4985a127f2604df6be528b774afcecb6
David Martos <davidignaciomartos@gmail.com>
2022-10-23 20:44:24 +0200

    Add errorHandler MW

--- index.js
+++ index.js
@@ -32,19 +32,20 @@ app.get('/info',(request,response) => {
     )
 })
 
-app.get('/api/persons/:id',(request,response) => {
+app.get('/api/persons/:id',(request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
             response.json(person)
         })
-    
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id',(request, response) => {
+app.delete('/api/persons/:id',(request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
         .then(person => {
             response.status(204).end()
         })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons',(request, response) => {
@@ -80,9 +81,43 @@ app.post('/api/persons',(request, response) => {
     })
 })
 
+app.put('/api/persons/:id',(request, response, next) => {
+    let body = request.body
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'The number are required'
+        })
+    }
+    Person.findByIdAndUpdate({_id: request.params.id}, {number: body.number }, {new: true})
+    .then(updatedPerson =>{
+        return response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 app.get('*', (request, response)  => {
     response.sendFile(path.resolve(__dirname, './build', 'index.html'));
-  });
+});
+
+// Error Middlewares
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
